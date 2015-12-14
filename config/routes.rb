492b4d8d90ba51878439d5d8590dd2c36573acdf
Jill Donohue <jilldonohue@Jills-MacBook-4.local>
2015-12-14 10:40:04 -0700

    updated contact info

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "/about", to: "welcome#about"
   get "/contact", to: "welcome#contact"
   get "/skills", to: "welcome#skills"
+  get "/portfolio", to: "welcome#portfolio"
 end
